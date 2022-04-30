d30ae7861d34ab8170f5d9f476e6a4acc2cb073a
yarda <yar.damir23@gmail.com>
2022-05-01 00:12:13 +0400

    1.0.0

--- app/src/main/java/com/example/itschool_project/MapActivity.java
+++ app/src/main/java/com/example/itschool_project/MapActivity.java
@@ -2,7 +2,11 @@ package com.example.itschool_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -10,17 +14,34 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class MapActivity extends AppCompatActivity {
 
-    private MapView mapView;
-    private GoogleMap map;
+    private WebView web;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mapView = findViewById(R.id.mapView);
 
-        mapView.onCreate(savedInstanceState);
+        WebView web = (WebView) findViewById(R.id.web);
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        web.getSettings().setDomStorageEnabled(true);
+        web.loadUrl("https://2gis.ru/izhevsk/search/%D0%BF%D1%80%D0%B8%D0%B5%D0%BC%20%D0%B2%D1%82%D0%BE%D1%80%D1%81%D1%8B%D1%80%D1%8C%D1%8F/rubricId/110523?m=53.220404%2C56.852085%2F12.67");
 
-        //mapView.getMapAsync(this);
     }
-}
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("www.example.com")) {
+                // Designate Urls that you want to load in WebView still.
+                return false;
+            }
+
+            // Otherwise, give the default behavior (open in browser)
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+
+
+}}
