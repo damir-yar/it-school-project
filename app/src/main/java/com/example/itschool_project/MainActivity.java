1cfc26b79a85cf4c69768f91d6cb3839274e5700
yarda <yar.damir23@gmail.com>
2022-06-01 19:22:41 +0400

    1.0.0

--- app/src/main/java/com/example/itschool_project/MainActivity.java
+++ app/src/main/java/com/example/itschool_project/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //getSupportActionBar().hide();
+        /*getSupportActionBar().hide();*/
 
         plast = findViewById(R.id.but_plast);
         met = findViewById(R.id.but_met);
